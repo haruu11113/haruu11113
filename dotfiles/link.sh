0deb4604a4c185b1a5fc1dc55289fc7910d46bc0
haruu11113 <haruu11113@gmail.com>
2020-09-30 15:28:04 +0900

    remove bash setting

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-dotfiles=(.tmux.conf .vimrc .zshrc .bashrc .bash_profile .emacs.d ide)
+dotfiles=( ".tmux.conf" ".vimrc" ".zshrc" ".emacs.d" "ide" )
 for f in ${dotfiles[@]}; do
     echo "リンク作成を開始します"
     if [ -e ~/${f} ]; then
         echo "./old/に${f}を移動"
         mv ~/${f} ./old/
-        echo "'~' に${f}のリンクを作成"
+        echo ""~" に${f}のリンクを作成"
         ln -s ${PWD}/${f} ~
         echo "======"
     else
-        echo "'~' に${f}のリンクを作成"
+        echo ""~" に${f}のリンクを作成"
         ln -s ${PWD}/${f} ~
         echo "====="
     fi
